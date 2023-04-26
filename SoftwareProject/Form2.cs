c3d52f4b9d5485e410feafa9ba8ab80add9c2922
Mohamedelshar78 <elsharkaweymohamed88@gmail.com>
2023-04-26 21:17:37 +0200

    disconnected mode finished

---
+++ SoftwareProject/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+namespace SoftwareProject
+{
+    public partial class Form2 : Form
+    {
+        string softconn = "Data Source=orcl;User Id=scott;Password=tiger;";
+        OracleConnection conn;
+        OracleDataAdapter adapter;
+        OracleCommandBuilder builder;
+        DataSet ds;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string cmdstr = "select * from organizer ";
+
+            adapter = new OracleDataAdapter(cmdstr, softconn);
+            ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            builder = new OracleCommandBuilder(adapter);
+            adapter.Update(ds.Tables[0]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
