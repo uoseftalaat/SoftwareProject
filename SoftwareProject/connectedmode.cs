fda85d5e97debc5722e1a1bada32aab9d6e2cae1
unknown <uoseftalaat0@gmail.com>
2023-04-28 12:05:18 -0700

    untested procuders for connected mode

--- SoftwareProject/connectedmode.cs
+++ SoftwareProject/connectedmode.cs
@@ -26,27 +26,10 @@ namespace SoftwareProject
         {
             InitializeComponent();
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         private void label2_Click(object sender, EventArgs e)
         {
 
         }
-
         private void connectedmode_Load(object sender, EventArgs e)
         {
             con = new OracleConnection(); ‏
@@ -55,24 +38,30 @@ namespace SoftwareProject
            
 
         }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
              ‏OracleCommand cmd = new OracleCommand();
             cmd.CommandText = "select date_,price from event where name=:event_name";
             cmd.CommandType = CommandType.Text;
             cmd.Parameters.Add("event_name", comboBox1.SelectedItem.ToString());
-
+            OracleCommand sel = new OracleCommand();
+            sel.CommandText = "GetEventName";
+            sel.CommandType = CommandType.StoredProcedure;
+            sel.Parameters.Add("events", OracleDbType.RefCursor, ParameterDirection.Output);
+            OracleDataReader dr2 = sel.ExecuteReader();
+            while (dr2.Read())
+            {
+                comboBox1.Items.Add(dr2[0]);
+            }
+            dr2.Close();
 
             OracleDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
@@ -83,19 +72,34 @@ namespace SoftwareProject
             dr.Close();
 
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            int max_id, new_id;
+            OracleCommand pro = new OracleCommand();
             OracleCommand cmd = new OracleCommand();
             cmd.Connection = con;
+            pro.Connection = con;
+            pro.CommandText = "GetTicketId";
+            pro.CommandType = CommandType.StoredProcedure;
+            pro.Parameters.Add("ticket_id",OracleDbType.Int32,ParameterDirection.Output);
+            pro.ExecuteNonQuery();
+            try
+            {
+                max_id = Convert.ToInt32(pro.Parameters["ticket_id"].Value.ToString());
+                new_id = max_id++;
+            }
+            catch
+            {
+                new_id = 1;
+            }
             cmd.CommandText = "insert into ticket values(:id,:event_id,:organizer_id)";
-            cmd.Parameters.Add("id", cmb_ID.Text);
+            cmd.Parameters.Add("id", new_id);
             cmd.Parameters.Add("event_id", comboBox1.Text);
             cmd.Parameters.Add("organizer_id", idbox.Text);
             int r = cmd.ExecuteNonQuery();
             if(r!= -1)
             {
-                //show ticket id
+                ticketbox.Text = new_id.ToString();
                 MessageBox.Show("BOOKED...");
             }
         }
